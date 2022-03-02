8172039645beacf83a38771d7fb45570be034d75
Mattias.Poppe <poppe.mattias@gmail.com>
2022-03-02 14:02:14 +0100

    DEV-1983 Add actix-web
    Implement two routes:
     - A simple GET health check
     - The event POST call returning the body for now

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, post, App, HttpResponse, HttpServer, Responder};
+
+
+#[get("/livez")]
+async fn livez() -> impl Responder {
+    HttpResponse::Ok()
+}
+
+#[post("/event")]
+async fn event(req_body: String) -> impl Responder {
+    HttpResponse::Ok().body(req_body)
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(livez)
+            .service(event)
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
