e790f9ad4be2e923f217ac5f413a872a9667a415
Mattias.Poppe <poppe.mattias@gmail.com>
2022-03-07 16:42:59 +0100

    DEV-1983 Add parsing of incoming events

---
+++ src/lib.rs
@@ -0,0 +1,125 @@
+use std::str;
+use chrono::{DateTime, Utc};
+
+use quick_xml::de::from_str;
+use serde::{Deserialize, Serialize};
+
+// XML structs
+#[derive(Serialize, Deserialize, Debug)]
+enum EventOutcomeStatus {
+    OK,
+    NOK,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Event {
+    #[serde(rename = "eventIdentifier")]
+    event_identifier: EventIdentifier,
+    #[serde(rename = "eventType")]
+    pub event_type: String,
+    #[serde(rename = "eventDateTime")]
+    pub event_timestamp: DateTime<Utc>,
+    #[serde(rename = "eventDetail")]
+    event_detail: String,
+    #[serde(rename = "eventOutcomeInformation")]
+    event_outcome_information: EventOutcomeInformation,
+    #[serde(rename = "linkingAgentIdentifier")]
+    linking_agent_identifier: LinkingAgentIdentifier,
+    #[serde(rename = "linkingObjectIdentifier")]
+    linking_object_identifier: Vec<LinkingObjectIdentifier>,
+    #[serde(skip_deserializing)]
+    pub event_payload: String,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct EventIdentifier {
+    #[serde(rename = "eventIdentifierType")]
+    event_identifier_type: String,
+    #[serde(rename = "eventIdentifierValue")]
+    event_identifier_value: String,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct EventOutcomeInformation {
+    #[serde(rename = "eventOutcome")]
+    event_outcome: String,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct LinkingAgentIdentifier {
+    #[serde(rename = "linkingAgentIdentifierType")]
+    linking_agent_identifier_type: String,
+    #[serde(rename = "linkingAgentIdentifierValue")]
+    linking_agent_identifier_value: String,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct LinkingObjectIdentifier {
+    #[serde(rename = "linkingObjectIdentifierType")]
+    linking_object_identifier_type: String,
+    #[serde(rename = "linkingObjectIdentifierValue")]
+    linking_object_identifier_value: String,
+}
+
+impl Event {
+    pub fn new(body: &str) -> Event {
+        // Deserialize XML to struct
+        let mut event: Event = from_str(body).unwrap();
+        // Add the body XMl as payload to the struct
+        event.event_payload = body.to_string();
+        return event;
+    }
+
+    pub fn to_xml(&self) -> String {
+        self.event_payload.clone()
+    }
+}
+#[cfg(test)]
+mod tests {
+    use std::str::FromStr;
+
+    use super::*;
+    #[test]
+    fn test_trigger_export_request() {
+        // Arrange
+        let body = r##"<premis:event xmlns:premis="info:lc/xmlns/premis-v2">
+            <premis:eventIdentifier>
+                <premis:eventIdentifierType>MEDIAHAVEN_EVENT</premis:eventIdentifierType>
+                <premis:eventIdentifierValue>111</premis:eventIdentifierValue>
+            </premis:eventIdentifier>
+            <premis:eventType>FLOW.ARCHIVED</premis:eventType>
+            <premis:eventDateTime>2019-03-30T05:28:40Z</premis:eventDateTime>
+            <premis:eventDetail>Ionic Defibulizer</premis:eventDetail>
+            <premis:eventOutcomeInformation>
+                <premis:eventOutcome>OK</premis:eventOutcome>
+            </premis:eventOutcomeInformation>
+            <premis:linkingAgentIdentifier>
+                <premis:linkingAgentIdentifierType>MEDIAHAVEN_USER</premis:linkingAgentIdentifierType>
+                <premis:linkingAgentIdentifierValue>703a53d2-dc66-4eb2-ab7f-73d5fd228852</premis:linkingAgentIdentifierValue>
+            </premis:linkingAgentIdentifier>
+            <premis:linkingObjectIdentifier>
+                <premis:linkingObjectIdentifierType>MEDIAHAVEN_ID</premis:linkingObjectIdentifierType>
+                <premis:linkingObjectIdentifierValue>a1b2c3</premis:linkingObjectIdentifierValue>
+            </premis:linkingObjectIdentifier>
+            <premis:linkingObjectIdentifier>
+                <premis:linkingObjectIdentifierType>EXTERNAL_ID</premis:linkingObjectIdentifierType>
+                <premis:linkingObjectIdentifierValue>a1</premis:linkingObjectIdentifierValue>
+            </premis:linkingObjectIdentifier>
+        </premis:event>"##;
+        // Act
+        let event = Event::new(body);
+        // Assert
+        assert_eq!(
+            event.event_type, "FLOW.ARCHIVED",
+        );
+        assert_eq!(
+            event.event_timestamp, DateTime::<Utc>::from_str("2019-03-30T05:28:40Z").unwrap(),
+        );
+        assert_eq!(
+            event.event_payload, body,
+        );
+        assert_eq!(
+            event.to_xml(), body,
+        );
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,40 @@
+use std::io::BufWriter;
+
 use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use xmltree::Element;
+
+use mh_events2pulsar::*;
 
 async fn livez() -> impl Responder {
     HttpResponse::Ok()
 }
 
 async fn event(req_body: String) -> impl Responder {
-    HttpResponse::Ok().body(req_body)
+    let xml_result = Element::parse(req_body.as_bytes());
+    match xml_result {
+        Ok(xml_tree) => {
+            // One ore more premis events are contained in an Events node.
+            for child in xml_tree.children {
+                if child.as_element().unwrap().name == "event" {
+                    // Write child element (= the premis event) to a String.
+                    let buf = Vec::new();
+                    let mut writer = BufWriter::new(buf);
+                    child.as_element().unwrap().write(&mut writer).unwrap();
+                    let premis_event_xml = String::from_utf8(writer.into_inner().unwrap()).unwrap();
+                    // Create the Event struct
+                    let premis_event = Event::new(&premis_event_xml);
+
+                    let xml = premis_event.to_xml();
+                    println!("{xml}");
+                }
+            }
+        }
+        Err(_e) => {
+            //handle_error(e);
+            //consumer.reject(delivery, false)?;
+        }
+    }
+    HttpResponse::Ok()
 }
 
 #[actix_web::main]
@@ -23,29 +52,61 @@ async fn main() -> std::io::Result<()> {
 #[cfg(test)]
 mod tests {
     use super::*;
-    use std::str::from_utf8;
     use actix_web::body::to_bytes;
     use actix_web::{test, web, App};
+    use std::str::from_utf8;
 
     #[actix_web::test]
     async fn test_livez() {
+        // Arrange
         let mut app = test::init_service(App::new().route("/livez", web::get().to(livez))).await;
+        // Act
         let req = test::TestRequest::with_uri("/livez").to_request();
         let resp = test::call_service(&mut app, req).await;
+        // Assert
         assert!(resp.status().is_success());
     }
 
     #[actix_web::test]
     async fn test_event() {
+        // Arrange
+        let body = r##"<events>
+            <premis:event xmlns:premis="info:lc/xmlns/premis-v2">
+            <premis:eventIdentifier>
+                <premis:eventIdentifierType>MEDIAHAVEN_EVENT</premis:eventIdentifierType>
+                <premis:eventIdentifierValue>111</premis:eventIdentifierValue>
+            </premis:eventIdentifier>
+            <premis:eventType>FLOW.ARCHIVED</premis:eventType>
+            <premis:eventDateTime>2019-03-30T05:28:40Z</premis:eventDateTime>
+            <premis:eventDetail>Ionic Defibulizer</premis:eventDetail>
+            <premis:eventOutcomeInformation>
+                <premis:eventOutcome>OK</premis:eventOutcome>
+            </premis:eventOutcomeInformation>
+            <premis:linkingAgentIdentifier>
+                <premis:linkingAgentIdentifierType>MEDIAHAVEN_USER</premis:linkingAgentIdentifierType>
+                <premis:linkingAgentIdentifierValue>703a53d2-dc66-4eb2-ab7f-73d5fd228852</premis:linkingAgentIdentifierValue>
+            </premis:linkingAgentIdentifier>
+            <premis:linkingObjectIdentifier>
+                <premis:linkingObjectIdentifierType>MEDIAHAVEN_ID</premis:linkingObjectIdentifierType>
+                <premis:linkingObjectIdentifierValue>a1b2c3</premis:linkingObjectIdentifierValue>
+            </premis:linkingObjectIdentifier>
+            <premis:linkingObjectIdentifier>
+                <premis:linkingObjectIdentifierType>EXTERNAL_ID</premis:linkingObjectIdentifierType>
+                <premis:linkingObjectIdentifierValue>a1</premis:linkingObjectIdentifierValue>
+            </premis:linkingObjectIdentifier>
+            </premis:event>
+        </events>"##;
+
         let mut app = test::init_service(App::new().route("/event", web::post().to(event))).await;
-        let req = test::TestRequest::post().uri("/event").set_payload("body").to_request();
+        // Act
+        let req = test::TestRequest::post()
+            .uri("/event")
+            .set_payload(body)
+            .to_request();
         let resp = test::call_service(&mut app, req).await;
+        // Assert
         assert!(resp.status().is_success());
         let body = to_bytes(resp.into_body()).await.unwrap();
-        assert_eq!(
-            "body",
-            from_utf8(&body).unwrap()
-        );
+        assert!(from_utf8(&body).unwrap().is_empty());
     }
 }
-
